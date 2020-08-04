e1f2b1f6ab004a7429749c381a78cd705eebc746
Bhushan Rajput <bhushanrajput2298@gmail.com>
2020-08-04 20:18:54 +0530

    Updates

---
+++ precache-manifest.507e73f6e4947af76b18adfb46bce674.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66cbefec9ec45f0daa090a0d85ca54b4",
+    "url": "/wizikey-assignment/index.html"
+  },
+  {
+    "revision": "86408332961c6c4d5609",
+    "url": "/wizikey-assignment/static/css/main.22e8882f.chunk.css"
+  },
+  {
+    "revision": "ea4774492ffa42934067",
+    "url": "/wizikey-assignment/static/js/2.7be1b996.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wizikey-assignment/static/js/2.7be1b996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86408332961c6c4d5609",
+    "url": "/wizikey-assignment/static/js/main.b8ae08b8.chunk.js"
+  },
+  {
+    "revision": "578a843f2fe92ac7da23",
+    "url": "/wizikey-assignment/static/js/runtime-main.dce6e68f.js"
+  }
+]);
